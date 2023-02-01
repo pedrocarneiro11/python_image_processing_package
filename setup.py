fa6dc3d9f4185c6b132d5eb322d8af3ea61b8899
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2023-02-01 16:42:33 -0300

    criando distribuicoes

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-with open("README,md", "r") as f:
+with open("README.md", "r") as f:
     page_description = f.read()
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_processing_package",
+    name="image_processing",
     version="0.0.1",
     author="Pedro Carneiro",
     author_email="pedrocarneirocunha87@gmail.com",
