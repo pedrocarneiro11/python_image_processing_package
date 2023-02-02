45b26f8f698b2a79bed34559d82c73ef23e326c2
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2023-02-02 12:27:25 -0300

    commit

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ with open("requirements.txt") as f:
 
 setup(
     name="image_processing",
-    version="0.0.1",
+    version="0.0.2",
     author="Pedro Carneiro",
     author_email="pedrocarneirocunha87@gmail.com",
     description="pacote de processamento de imagens em Python",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    # url="https://github.com/pedrocarneiro11/python_image_processing_package",
+    url="https://github.com/pedrocarneiro11/python_image_processing_package",
+    Homepage="https://github.com/pedrocarneiro11/python_image_processing_package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
